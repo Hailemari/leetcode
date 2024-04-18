a070432784a159db6d7ca1d4e5aec117d72d6f32
Hailemariam Kefale <95341678+Hailemari@users.noreply.github.com>
2024-04-18 19:29:45 +0300

    Created 'Course Schedule II' by A2SV Hub

---
+++ 18-Apr-2024/Course Schedule II 127059.py
@@ -0,0 +1,40 @@
+# Problem: Course Schedule II - https://leetcode.com/problems/course-schedule-ii/
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = [[] for _ in range(numCourses)]
+        order = []
+
+        for course, pre_requisite in prerequisites:
+            graph[pre_requisite].append(course)
+
+        WHITE = 0
+        GRAY = 1
+        BLACK = 2
+        colors = [WHITE for _ in range(numCourses)]
+
+        def dfs(node):
+            if colors[node] == GRAY:
+                return False
+           
+            if colors[node] == BLACK:
+                return True
+            
+            colors[node] = GRAY
+            for neighbor in graph[node]:
+                if not dfs(neighbor):
+                    return False
+
+            colors[node] = BLACK
+            order.append(node)
+
+            return True
+
+        for i in range(numCourses):
+            if colors[i] == WHITE:
+                if not dfs(i):
+                    return []
+
+        return order[::-1]
+
+

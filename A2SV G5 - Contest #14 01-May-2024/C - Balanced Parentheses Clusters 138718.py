f039c00b9a9e41eb400686c7ee17d3c79693d5d5
Hailemariam Kefale <95341678+Hailemari@users.noreply.github.com>
2024-05-01 21:58:15 +0300

    Created 'C - Balanced Parentheses Clusters' by A2SV Hub

---
+++ A2SV G5 - Contest #14 01-May-2024/C - Balanced Parentheses Clusters 138718.py
@@ -0,0 +1,12 @@
+# Problem: C - Balanced Parentheses Clusters - https://codeforces.com/gym/520688/problem/C
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    s = input()
+    ans = 1
+    for i in range(1, 2 * n):
+        if s[i] == "(" and s[i - 1] == "(":
+            ans += 1
+
+    print(ans)

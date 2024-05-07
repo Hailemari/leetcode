6e534718a0e128a7b04ef2316b7e2c86b38aaf70
Hailemariam Kefale <95341678+Hailemari@users.noreply.github.com>
2024-05-07 18:22:40 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 07-May-2024/Cirno's Perfect Bitmasks Classroom 142580.py
@@ -0,0 +1,14 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+t = int(input())
+
+for _ in range(t):
+    x = int(input())
+    
+    y = x & -x
+    if y ^ x:
+        print(y)
+    else:
+        x = ~x
+        z = x & -x
+        print(y + z)

5960c7696e77688bae338c6cadcaac6b6fc0ac03
Hailemariam Kefale <95341678+Hailemari@users.noreply.github.com>
2024-05-23 09:43:23 +0300

    Created 'B - Grid Path' by A2SV Hub

---
+++ A2SV G5 - Contest #17 23-May-2024/B - Grid Path 157698.py
@@ -0,0 +1,47 @@
+# Problem: B - Grid Path - https://codeforces.com/gym/524965/problem/B
+
+import sys
+import threading
+
+
+input = sys.stdin.readline
+
+def main():
+    t = int(input())
+    for _ in range(t):
+        m, n, k = map(int, input().split())
+        
+        memo = {}
+        
+        def f(i, j):
+            if (i, j) in memo:
+                return memo[(i, j)]
+            
+            if i == m - 1 and j == n - 1:
+                return 0
+            if i >= m or j >= n:
+                return float('-inf')
+            
+           
+            down = (j + 1) + f(i + 1, j)
+            right = (i + 1) + f(i, j + 1)
+            
+
+            if down == k or right == k:
+                result = k
+            else:
+                result = max(down, right)
+            memo[(i, j)] = result
+            return result
+        
+        if f(0, 0) == k:
+            print('YES')
+        else:
+            print('NO')
+
+if __name__ == '__main__':
+    sys.setrecursionlimit(10000)  
+    threading.stack_size(67108864) 
+    main_thread = threading.Thread(target=main)
+    main_thread.start()
+    main_thread.join()

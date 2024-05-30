201a1a4e770f3a55fe0fc5a65ac5cb935e6d0b71
Hailemariam Kefale <95341678+Hailemari@users.noreply.github.com>
2024-05-30 09:37:43 +0300

    Created 'A - Ascend' by A2SV Hub

---
+++ A2SV G5 - Contest #18 30-May-2024/A - Ascend 162062.py
@@ -0,0 +1,16 @@
+# Problem: A - Ascend - https://codeforces.com/gym/526229/problem/A
+
+n = int(input())
+elements = list(map(int, input().split()))
+
+max_length = 1
+sub_array = [-1]
+
+for element in elements:
+    if element > sub_array[-1]:
+        sub_array.append(element)
+        max_length = max(max_length, len(sub_array) - 1)
+    else:
+        sub_array = [-1, element]
+
+print(max_length)

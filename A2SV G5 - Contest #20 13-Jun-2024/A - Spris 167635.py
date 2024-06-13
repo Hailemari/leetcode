3ffb57b28b4934616245b879be8ef3750ef328dc
Hailemariam Kefale <95341678+Hailemari@users.noreply.github.com>
2024-06-13 08:55:29 +0300

    Created 'A - Spris' by A2SV Hub

---
+++ A2SV G5 - Contest #20 13-Jun-2024/A - Spris 167635.py
@@ -0,0 +1,13 @@
+# Problem: A - Spris - https://codeforces.com/gym/528792/problem/A
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+ans = 0
+for i in range(a, 0, -1):
+    if 2 * i <= b and 4 * i <= c:
+        ans = i + 2 * i + 4 * i
+        break
+
+print(ans)

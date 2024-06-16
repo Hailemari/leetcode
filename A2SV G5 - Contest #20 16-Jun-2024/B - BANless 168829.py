32e2bfe51f68d7eb0e439edea1a7b7549229cef2
Hailemariam Kefale <95341678+Hailemari@users.noreply.github.com>
2024-06-16 09:01:07 +0300

    Created 'B - BANless' by A2SV Hub

---
+++ A2SV G5 - Contest #20 16-Jun-2024/B - BANless 168829.py
@@ -0,0 +1,15 @@
+# Problem: B - BANless - https://codeforces.com/gym/528792/problem/B
+
+for _ in range(int(input())):
+        n = int(input())
+        pairs = []
+        
+        start, end = 1, 3 * n 
+        while start < end:
+            pairs.append([start, end])
+            start += 3
+            end -= 3    
+
+        print(len(pairs))
+        for pair in pairs:
+            print(*pair) 

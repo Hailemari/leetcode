3bfb381db4e3854683b9ef116dce35699bd51482
Hailemariam Kefale <95341678+Hailemari@users.noreply.github.com>
2024-06-21 18:12:19 +0300

    Created 'A - abbccc' by A2SV Hub

---
+++ A2SV G5 - Contest #21 21-Jun-2024/A - abbccc 171073.py
@@ -0,0 +1,15 @@
+# Problem: A - abbccc - https://codeforces.com/gym/530187/problem/A
+
+n = int(input())
+t = input()
+
+jump = 1
+i = 0
+res = ""
+
+while i < n:
+    res += t[i]
+    i += jump
+    jump += 1
+
+print(res)

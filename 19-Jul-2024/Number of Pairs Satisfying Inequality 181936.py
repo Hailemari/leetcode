d89545cca618f1b9e870a50fb11edc5027a3ff86
Hailemariam Kefale <95341678+Hailemari@users.noreply.github.com>
2024-07-19 22:04:02 +0300

    Created 'Number of Pairs Satisfying Inequality' by A2SV Hub

---
+++ 19-Jul-2024/Number of Pairs Satisfying Inequality 181936.py
@@ -0,0 +1,13 @@
+# Problem: Number of Pairs Satisfying Inequality - https://leetcode.com/problems/number-of-pairs-satisfying-inequality/
+
+class Solution:
+    def numberOfPairs(self, nums1: List[int], nums2: List[int], diff: int) -> int:
+        arr = []
+        pairs = 0
+        n = len(nums1)
+        for i in range(n):
+            cur = bisect_right(arr , nums1[i] - nums2[i] + diff)
+            pairs += cur
+            bisect.insort(arr , nums1[i] - nums2[i])
+
+        return pairs

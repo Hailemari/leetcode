286ba621887376cb57be4c6219f527fd1429d09e
Hailemariam Kefale <95341678+Hailemari@users.noreply.github.com>
2024-08-17 23:00:56 +0300

    Created 'Integer Break' by A2SV Hub

---
+++ 17-Aug-2024/Integer Break 197536.py
@@ -0,0 +1,17 @@
+# Problem: Integer Break - https://leetcode.com/problems/integer-break/
+
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        if n == 2:
+            return 1
+        if n == 3:
+            return 2
+        
+        product = 1
+        while n > 4:
+            product *= 3
+            n -= 3
+    
+        product *= n
+        
+        return product

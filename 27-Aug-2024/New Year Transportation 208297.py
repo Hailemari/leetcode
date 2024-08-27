ab42761456631fae0222272e1d885771324652e2
Hailemariam Kefale <95341678+Hailemari@users.noreply.github.com>
2024-08-27 17:29:14 +0300

    Created 'New Year Transportation' by A2SV Hub

---
+++ 27-Aug-2024/New Year Transportation 208297.py
@@ -0,0 +1,15 @@
+# Problem: New Year Transportation - https://codeforces.com/problemset/problem/500/A
+
+n, t = map(int, input().split())
+arr = list(map(int, input().split()))
+
+i = 1
+ans = "NO"
+while i <= t:
+    if i == t:
+        ans = "YES"
+        break
+    
+    i += arr[i - 1]
+
+print(ans)

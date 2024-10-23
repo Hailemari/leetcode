ccc8e39ccaa22813f838fe8f247f16dfa74a2e74
Hailemariam Kefale <95341678+Hailemari@users.noreply.github.com>
2024-10-23 13:25:09 +0300

    Created 'First Bad Version' by A2SV Hub

---
+++ 23-Oct-2024/First Bad Version 221745.py
@@ -0,0 +1,19 @@
+# Problem: First Bad Version - https://leetcode.com/problems/first-bad-version/
+
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        low = 1
+        high = n
+        
+        while low < high:
+            mid = low + (high - low) // 2  
+            if isBadVersion(mid):
+                high = mid  
+            else:
+                low = mid + 1 
+        
+        first_bad = low
+        return first_bad
